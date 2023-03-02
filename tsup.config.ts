4dc6789a946363fef74b1b70430fdaa1a40bbaa1
Ben Koplin <benjaminkoplin@outlook.com>
2023-03-02 07:25:23 -0600

    fix: add global name

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['./src/index.ts'],
   format: ['esm', 'iife', 'cjs'],
+  globalName: 'dateFns',
   clean: true,
   dts: {
     entry: ['./src/index.ts'],
